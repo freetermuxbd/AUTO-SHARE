523ba6f273e2c3bffd397043922a7f800f4be2e7
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-07 23:59:37 +0600

    Update run.py

--- run.py
+++ run.py
@@ -7,5 +7,4 @@ if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
     import share
- else:
-    exit('\033[1;31m[×] Sorry Device Not Support')
+    share.bot_share()
